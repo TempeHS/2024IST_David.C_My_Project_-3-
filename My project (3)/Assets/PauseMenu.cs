ca27853af672185cc6bbedd6513e82398359b7e9
DavidChi321 <158530688+DavidChi321@users.noreply.github.com>
2024-06-04 10:10:39 +1000

    FINSIHED PAUSE MENU

--- My project (3)/Assets/PauseMenu.cs
+++ My project (3)/Assets/PauseMenu.cs
@@ -6,21 +6,25 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] GameObject pauseMenu;
     public void Pause()
     {
-        pauseMenu.Setactive(true);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
     
     public void restart()
     {
-        SceneManagement.LoadScene(SceneManager.GetActiveScene().buildindex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
     }
 }
