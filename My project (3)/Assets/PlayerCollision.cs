b0024addfcd786d1951174f8d3f4e853e1a39d72
DavidChi321 <158530688+DavidChi321@users.noreply.github.com>
2024-09-24 09:39:11 +1000

    a

--- My project (3)/Assets/PlayerCollision.cs
+++ My project (3)/Assets/PlayerCollision.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-
  
    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.tag == "Obstacle")
         {
-            Destroy(other.gameObject);
+            Destroy(gameObject);
         }
 
     }
